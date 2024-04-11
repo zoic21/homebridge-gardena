dc3b438e2212d811d8c9ce62b6869b653083a4fe
Loïc <1536036+zoic21@users.noreply.github.com>
2024-04-11 12:46:09 +0200

    test

--- src/platform.ts
+++ src/platform.ts
@@ -2,7 +2,7 @@ import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, 
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { StatePlatformAccessory } from './statePlatformAccessory';
-import { GardenaConnection} from 'gardena-smart-system';
+import GardenaConnection from 'gardena-smart-system';
 
 
 /**
